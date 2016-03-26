eb8a596c15630ba934c92a5a8b731fe4787732ea
Ryan Hoffman <rhoffman@Ryans-iMac.local>
2016-03-25 22:48:00 -0400

    working UIPageViewController

--- CBT Velocity/ViewController.swift
+++ CBT Velocity/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPageViewControllerDataSource {
+    
+    var pageViewController: UIPageViewController!
+    var pageTitles: NSArray!
+    var pageImages: NSArray!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.pageTitles = NSArray(objects: "Activities", "About Us")
+        self.pageImages = NSArray(objects: "Insomnia", "Velocity")
+        
+        self.pageViewController = self.storyboard?.instantiateViewControllerWithIdentifier("PageViewController") as! UIPageViewController
+        
+        self.pageViewController.dataSource = self
+        
+        let startVC = self.viewControllerAtIndex(0) as ContentViewController
+        let viewControllers = NSArray(object: startVC)
+        
+        self.pageViewController.setViewControllers(viewControllers as? [UIViewController], direction: .Forward, animated: true, completion: nil)
+        
+        self.pageViewController.view.frame = CGRect(x: 0, y: 30, width: self.view.frame.width, height: self.view.frame.size.height - 60)
+        
+        self.addChildViewController(self.pageViewController)
+        self.view.addSubview(self.pageViewController.view)
+        self.pageViewController.didMoveToParentViewController(self)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,58 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func viewControllerAtIndex(index: Int) -> ContentViewController {
+        // Return an empty ContentViewController.
+        if((self.pageTitles.count == 0) || (index >= self.pageTitles.count)){
+            return ContentViewController()
+        }
+        
+        // Assign the correct title to the top and the correct image as the main content.
+        let vc: ContentViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ContentViewController") as! ContentViewController
+        vc.imageFile = self.pageImages[index] as! String
+        vc.titleText = self.pageTitles[index] as! String
+        vc.pageIndex = index
+        
+        return vc
+    }
+    
+    // MARK: Page View Controller Data Source
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        let vc = viewController as! ContentViewController
+        var index = vc.pageIndex as Int
+        
+        if (index == 0 || index == NSNotFound) {
+            return nil
+        }
+        
+        index -= 1
+        return self.viewControllerAtIndex(index)
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        let vc = viewController as! ContentViewController
+        var index = vc.pageIndex as Int
+        
+        index += 1
+        if (index == NSNotFound) {
+            return nil
+        }
+        
+        if (index == self.pageTitles.count) {
+            return nil
+        }
+        
+        return self.viewControllerAtIndex(index)
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return self.pageTitles.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
 
 }
 
