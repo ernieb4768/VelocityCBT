549ea7150e70b88082ff34428661ed205d379c88
Ryan Hoffman <rhoffman@Ryans-iMac.local>
2016-04-05 21:07:54 -0400

    add UITableView

---
+++ CBT Velocity/ActivitiesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActivitiesTableViewCell.swift
+//  CBT Velocity
+//
+//  Created by Ryan Hoffman on 4/5/16.
+//  Copyright © 2016 Ryan Hoffman. All rights reserved.
+//
+
+import UIKit
+
+class ActivitiesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
